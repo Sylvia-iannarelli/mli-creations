f38bd267e5acd54c0bb1e5ace727839960fcdcf2
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-05-11 19:04:02 +0200

    Ajout JWT sur API (à finaliser pour compte et commandes)

--- TODO.md
+++ TODO.md
@@ -1,8 +1,9 @@
 # Liste des développements à effectuer
 
+- Gestion des messages d'erreur sur les routes API
+- Routes API
+- JWT & CORS
+- Reset du password
 - Flash messages
 - Service mailing
 - Subscriber pour maintenance
-- Reset du password
-- Routes API
-- JWT & CORS
--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -7,16 +7,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/api/users", name="app_api_user_getUsers", methods={"GET"})
+     * @IsGranted("ROLE_ADMIN")
      */
     public function getUsers(UserRepository $userRepository): JsonResponse
     {
-        $users = $userRepository->findAll();
+        // $users = $userRepository->findAll();
 
-        return $this->json($users, Response::HTTP_OK, [], ["groups" => "user_index"]);
+        // return $this->json($users, Response::HTTP_OK, [], ["groups" => "user_index"]);
     }
 }
