fe3be6164480a9d339d6ced708394522df70624b
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-05-13 15:11:47 +0200

    Api/UserController en attente

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -11,14 +11,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/api/users", name="app_api_user_getUsers", methods={"GET"})
-     * @IsGranted("ROLE_ADMIN")
-     */
-    public function getUsers(UserRepository $userRepository): JsonResponse
-    {
-        // $users = $userRepository->findAll();
+    // /**
+    //  * @Route("/api/users", name="app_api_user_getUsers", methods={"GET"})
+    //  * @IsGranted("ROLE_ADMIN")
+    //  */
+    // public function getUsers(UserRepository $userRepository): JsonResponse
+    // {
+    //     $users = $userRepository->findAll();
 
-        // return $this->json($users, Response::HTTP_OK, [], ["groups" => "user_index"]);
-    }
+    //     return $this->json($users, Response::HTTP_OK, [], ["groups" => "user_index"]);
+    // }
 }
